a90100edf619e894609eb19291947871f40b5156
Jeebu0332 <109778352+JeewanAidi@users.noreply.github.com>
2023-01-18 04:55:55 -0500

    Check either positive or negative in C++

---
+++ pg2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter the number : "<<endl;
+    cin>>a;
+
+    if(a>0){
+        cout<<"The given number is positive ."<<endl;
+    }
+    else{
+        cout<<"The number is negative ."<<endl;
+    }
+    return 0;
+}
