b03caa6d99fbe2564c840b5bcfabfc25a0889192
Jeebu0332 <109778352+JeewanAidi@users.noreply.github.com>
2023-01-20 02:49:50 -0500

    Multiplication using C++ code

---
+++ multiplication.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,c;
+	cout<<"Enter the value of and b"<<endl;
+	cin>>a>>b;
+	c=a*b;
+	cout<<"The multiplication of a and b is "<<c<<endl;
+	return 0;
+}
